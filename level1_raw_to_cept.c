35d56e1575496be494e9b8a2d08e37bbb4946b58
casandro <christian@clarke-3.d>
2019-11-02 10:09:35 +0100

    Fixed problem with ß character

--- level1_raw_to_cept.c
+++ level1_raw_to_cept.c
@@ -7,9 +7,17 @@
 
 #define PLEN 40
 
-void print_btx_char(uint8_t d, int lang)
+//Mosaik is pointer to integer if *mosaik==1 => graphics active, no translation
+
+void print_btx_char(uint8_t d, int lang, int * mosaik)
 {
 	d=d&0x7f;
+	if (d<0x20) {
+		printf("%c", d+0x80); 
+		if ((d>=0x10) && (d<=0x17)) *mosaik=1;
+		if ((d>=0x18) && (d<=0x1F)) *mosaik=0;
+	}else
+	if (*mosaik==1) printf("%c", d); else
 	//#
 	if (d==0x23) printf("\x23"); else
 	//$
@@ -27,7 +35,7 @@ void print_btx_char(uint8_t d, int lang)
 	//_
 	if (d==0x5f) printf("\x5f"); else 
 	//°
-	if ( (d==0x60) && (lang!=-1)) printf("\x19\x30"); else 
+	if ( (d==0x60) ) printf("\x19\x30"); else 
 	//ä
 	if (d==0x7b) printf("\x19Ha"); else
 	//ö
@@ -35,15 +43,15 @@ void print_btx_char(uint8_t d, int lang)
 	//ü
 	if (d==0x7d) printf("\x19Hu"); else
 	//ß
-	if (d==0x7e) printf("\x1d;"); else 
-	if (d<0x20) printf("%c",d+0x80) ;
-	else printf("%c",d);
+	if (d==0x7e) printf("\x19\x7b"); else 
+	printf("%c",d);
 }
 
 void print_line(int row, uint8_t *data)
 {
-	int col=0;
-	if (row==0) col=8;
+	int col=0;	
+	printf("\x87"); //kill features, workaround for some buggy terminals not clearing attributes on line end
+	int mosaik=0;
 	printf("\x1f%c%c",0x41+row,0x41+col);
 	int n;
 	int cnt=0;
@@ -54,30 +62,29 @@ void print_line(int row, uint8_t *data)
 			if (ac>=0) {
 				if (ac<0x20) {
 					int m;
-					for (m=0; m<cnt; m++) print_btx_char(ac,-1);
+					for (m=0; m<cnt; m++) print_btx_char(ac,-1,&mosaik);
 				} else
-				if (cnt==1) print_btx_char(ac,-1); 
+				if (cnt==1) print_btx_char(ac,-1, &mosaik); 
 				else {
 					int cnt_tmp=cnt;
 					while (cnt_tmp>0) {	
 						int dec=cnt_tmp;
 						if (dec>40) dec=40;
-						print_btx_char(ac,-1);
+						print_btx_char(ac,-1, &mosaik);
 						printf("\x12%c", 0x41+dec-2);
 						cnt_tmp=cnt_tmp-dec;
 					}
 					int m;
-					for (m=0; m<cnt_tmp; m++) print_btx_char(ac,-1);
+					for (m=0; m<cnt_tmp; m++) print_btx_char(ac,-1,&mosaik);
 				}
 			}
-			if (n==39) print_btx_char(c,-1);
+			if (n==39) print_btx_char(c,-1,&mosaik);
 			ac=c;
 			cnt=1;
 		} else {
 			cnt=cnt+1; //count repetitions
 		}
 	}
-	printf("\x87"); //kill features
 }
 
 
@@ -86,8 +93,9 @@ int main(int argc, char *argv[])
 {
 	uint8_t buf[PLEN];
 	int row=0;
+	printf("\x1F\x2D\x71");
 	printf("\x1F\x2F\x41"); //reset terminal to serial attributes
-	for (row=0; row<25; row++) {
+	for (row=0; row<24; row++) {
 		if (read(0, buf, PLEN)==PLEN) {
 			print_line(row, buf);
 		}
